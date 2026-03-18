cc90a08118f9b43b1030039025e118bc2b016359
VINICIUS DE SOUZA ARAUJO <47627451892@info.local>
2026-03-17 22:07:26 -0300

    Commit da ultima branch, de relatorio junto com testes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-# Esquema da lista de tupla: [("Nome", [notas])]
+# Esquema da lista de tuplas: [("Nome", [notas])]
 alunos_senai = [
     ("Vinicius", [8.5, 9.0, 7.5]),
     ("Pedro", [6.0, 5.5, 4.0]),
@@ -11,3 +11,33 @@ alunos_senai = [
     ("Ana", []),        # Lista vazia para teste
     ("João", [7.0, "erro", 8.0]) # Lista de dados corrompidos para teste
 ]
+from processamento_dados import filtrar_recuperacao, aluno_destaque, gerar_relatorio
+
+
+alunos_senai = [
+    ("Vinicius", [8.5, 9.0, 7.5]),
+    ("Pedro", [6.0, 5.5, 4.0]),
+    ("Fulana", [5.0, 9.5, 4.0]),
+    ("Roberta", [7.0, 9.5, 3.5]),
+    ("Maria Aparecida", [9.8, 4.5, 5.5]),
+    ("Alice", [4.0, 6.5, 7.0]),
+    ("Maria", [10.0, 9.5]),
+    ("Ana", []),        
+    ("João", [7.0, "erro", 8.0]) 
+]
+
+
+if __name__ == "__main__":
+    
+    recuperacao = filtrar_recuperacao(alunos_senai)
+    
+    
+    destaque = aluno_destaque(alunos_senai)
+    
+    
+    gerar_relatorio(recuperacao, destaque)
+    
+    
+    print("Processamento finalizado com sucesso!")
+    print(f"Alunos em recuperação encontrados: {len(recuperacao)}")
+    print("Arquivo 'resultado.txt' criado.")
--- processamento_dados.py
+++ processamento_dados.py
@@ -1,9 +1,16 @@
 def calcular_media(notas):
-
-    if not notas:
+    
+    notas_validas = []
+    for n in notas:
+        if isinstance(n, (int, float)):
+            notas_validas.append(n)
+    
+    
+    if not notas_validas:
         return 0.0
     
-    return sum(notas)/ len(notas)
+    
+    return sum(notas_validas) / len(notas_validas)
 
 def filtrar_recuperacao(lista_alunos):
     alunos_recuperacao = []
@@ -17,7 +24,7 @@ def gerar_relatorio(recuperacao, destaque, nome_arquivo="resultado.txt"):
     with open(nome_arquivo, "w", encoding="utf-8") as arquivo:
         arquivo.write("--- RELATÓRIO ACADÊMICO SENAI ---\n\n")
         
-        # Escrevendo o Aluno Destaque
+        
         if destaque:
             nome_top, media_top = destaque
             arquivo.write(f"DESTAQUE: {nome_top} com média {media_top:.1f}\n")
@@ -25,13 +32,13 @@ def gerar_relatorio(recuperacao, destaque, nome_arquivo="resultado.txt"):
         arquivo.write("-" * 30 + "\n")
         arquivo.write("ALUNOS EM RECUPERAÇÃO:\n")
         
-        # Escrevendo a lista de recuperação
+        
         if not recuperacao:
             arquivo.write("Nenhum aluno em recuperação.\n")
         else:
             for nome, media in recuperacao:
                 arquivo.write(f"- {nome}: {media:.1f}\n")
-                
+
 def aluno_destaque(lista_alunos):
     melhor_aluno = None
     maior_media = -1.0 
