fce8a37c7362f75b91b34ac1bb53ea0dc99272be
VINICIUS DE SOUZA ARAUJO <47627451892@info.local>
2026-03-17 22:09:47 -0300

    Commit da ultima branch, de relatorio junto com testes e codigo final

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-
-# Esquema da lista de tuplas: [("Nome", [notas])]
-alunos_senai = [
-    ("Vinicius", [8.5, 9.0, 7.5]),
-    ("Pedro", [6.0, 5.5, 4.0]),
-    ("Fulana", [5.0, 9.5, 4.0]),
-    ("Roberta", [7.0, 9.5, 3.5]),
-    ("Maria Aparecida", [9.8, 4.5, 5.5]),
-    ("Alice", [4.0, 6.5, 7.0]),
-    ("Maria", [10.0, 9.5]),
-    ("Ana", []),        # Lista vazia para teste
-    ("João", [7.0, "erro", 8.0]) # Lista de dados corrompidos para teste
-]
 from processamento_dados import filtrar_recuperacao, aluno_destaque, gerar_relatorio
 
 
